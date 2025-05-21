19269c4e77f47f5f82e19b8d2eff0288a43af1ad
Venki8500 <cherryvenki8500@gmail.com>
2025-05-21 19:59:26 +0530

    abc

--- src/components/ManagerDashboard.jsx
+++ src/components/ManagerDashboard.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link, useNavigate } from "react-router";
+import { useNavigate } from "react-router";
 import { useGetDataQuery, useDownpaymentReciveMutation } from "../services/LmsApi";
 
 function ManagerDashboard() {
